42bd71e607f192bd6c8f3ea707410d93f45c4e5d
nginnever <ginneversource@gmail.com>
2019-12-04 16:17:10 -0800

    remove some things

--- build/depends/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/newton/github/test/FFT-Multiexp-AMD/depends
-# Build directory: /home/newton/github/test/FFT-Multiexp-AMD/build/depends
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
